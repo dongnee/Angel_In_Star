9f8eb04621b27f034e73555c9840161af2f810a8
irina0627 <seyeon0627@gmail.com>
2022-02-11 15:27:22 +0900

    add menu

--- js/menu.js
+++ js/menu.js
@@ -6,16 +6,37 @@ $(function(){
 
     $('#menu li:nth-child(2)').on('click',function(){
         var offset = $('#beverage').offset();
+<<<<<<< Updated upstream
         $('html').animate({scrollTop:offset.top},600);
+=======
+        $('html').animate({scrollTop:offset.top},800);
+>>>>>>> Stashed changes
     });
 
     $('#menu li:nth-child(3)').on('click',function(){
         var offset = $('#bread').offset();
+<<<<<<< Updated upstream
         $('html').animate({scrollTop:offset.top},800);
+=======
+        $('html').animate({scrollTop:offset.top},1200);
+>>>>>>> Stashed changes
     });
 
     $('#menu li:nth-child(4)').on('click',function(){
         var offset = $('#icecream').offset();
+<<<<<<< Updated upstream
         $('html').animate({scrollTop:offset.top},1000);
+=======
+        $('html').animate({scrollTop:offset.top},1600);
+    });
+
+    $(window).on('scroll',function(){
+        if($(document).scrollTop() >= ($('#headerBox').height()+$('#mainMenuBox').height())){
+            $('#menuProduct').addClass('menuBoxFixed'); /*menu.css에 있음*/
+        }
+        else{
+            $('#menuProduct').removeClass('menuBoxFixed');
+        }
+>>>>>>> Stashed changes
     });
 });
